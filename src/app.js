0040b746c31be1990b621e8580ea855b451f616e
retori8 <reciclee@gmail.com>
2023-01-26 17:49:33 +0000

    nombres dominio finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let getNames = () => {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+
+    for (let i = 0; i < noun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < pronoun.length; k++) {
+          document.write(pronoun[k] + adj[j] + noun[i] + "." + "com" + "<br>");
+        }
+      }
+    }
+  };
+  document.querySelector("#namesId").innerHTML = getNames();
 };
